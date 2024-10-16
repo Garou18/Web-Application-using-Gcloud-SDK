8bc8bad217b2096f112b99f1cda34a5a743da1db
Boaz <98589978+Garou18@users.noreply.github.com>
2024-10-16 20:55:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import json
-import urllib
+import urllib.request  # Use `urllib.request` for Python 3
 import webapp2
 from google.appengine.ext.webapp import template
 
@@ -19,26 +19,34 @@ class MainPage(webapp2.RequestHandler):
             }
             path = os.path.join(os.path.dirname(__file__), 'index.html')
             return self.response.out.write(template.render(path, template_values))
-        url = "https://api.postalpincode.in/pincode/" + pincode
-        data = urllib.urlopen(url).read()
-        data = json.loads(data)
-        if(data[0]['Status'] == 'Success'):
-            post_office = data[0]['PostOffice'][0]['State']
-            name = data[0]['PostOffice'][0]['Name']
-            block = data[0]['PostOffice'][0]['Block']
-            district = data[0]['PostOffice'][0]['District']
-            template_values = {
-                "post_office": post_office,
-                "name": name,
-                "block": block,
-                "district": district
-            }
-            path = os.path.join(os.path.dirname(__file__), 'results.html')
-            self.response.out.write(template.render(path, template_values))
-        else:
-            template_values = {}
-            path = os.path.join(os.path.dirname(__file__), 'error.html')
-            self.response.out.write(template.render(path, template_values))
+        
+        try:
+            url = "https://api.postalpincode.in/pincode/" + pincode
+            with urllib.request.urlopen(url) as response:  # Use 'with' for better resource management
+                data = response.read()
+                data = json.loads(data)
+
+            if data[0]['Status'] == 'Success':
+                post_office = data[0]['PostOffice'][0]['State']
+                name = data[0]['PostOffice'][0]['Name']
+                block = data[0]['PostOffice'][0]['Block']
+                district = data[0]['PostOffice'][0]['District']
+                
+                template_values = {
+                    "post_office": post_office,
+                    "name": name,
+                    "block": block,
+                    "district": district
+                }
+                path = os.path.join(os.path.dirname(__file__), 'results.html')
+                self.response.out.write(template.render(path, template_values))
+            else:
+                template_values = {}
+                path = os.path.join(os.path.dirname(__file__), 'error.html')
+                self.response.out.write(template.render(path, template_values))
+        except Exception as e:
+            # Log the error for debugging
+            self.response.out.write(f"An error occurred: {str(e)}")
 
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
